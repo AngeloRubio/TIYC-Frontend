a9eb0cbf92b8db0a18851d2ed640d7a53346531b
AngeloRubio <angelo.rubiomer@ug.edu.ec>
2025-07-11 15:22:14 -0500

    vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-servidor-produccion.com/api',
-  appName: 'TIYC - Tú Inspiras, Yo Creo',
-  version: '1.0.0'
+  apiUrl: 'https://tiyc-backend.up.railway.app/api'
 };
