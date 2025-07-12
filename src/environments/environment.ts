4404b50da3d142bca437e966faad7a9a4d91d48a
AngeloRubio <angelo.rubiomer@ug.edu.ec>
2025-07-12 11:50:55 -0500

    prueba environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:5000/api',
+//   appName: 'TIYC - Tú Inspiras, Yo Creo'
+// };
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://tiyc-backend-production.up.railway.app/api',  // ← CAMBIO AQUÍ
   appName: 'TIYC - Tú Inspiras, Yo Creo'
 };
